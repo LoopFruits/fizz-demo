b850ef5d0b5669a44b60496fc766201700741c57
LoopFruits <Kensworldc@gmail.com>
2024-12-30 13:34:04 -0500

    added GSAP animation to heading

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -4,15 +4,15 @@ import clsx from 'clsx';
 
 
 type Props = {
-    buttonLink: LinkField;
-    buttonText: string | null;
-    className: string;
+  buttonLink: LinkField;
+  buttonText: string | null;
+  className: string;
 }
 
 export default function Button({buttonLink,buttonText,className}: Props) {
   return (
     <PrismicNextLink 
-        className={clsx("", className)}
+        className={clsx("rounded-xl bg-orange-600 px-5 py-4 text-center text-xl font-bold uppercase tracking-wide text-white transition-colors duration-150  hover:bg-orange-700 md:text-2xl", className)} // classname styling passed in from button element in index.tsx
     field={buttonLink}>
         {buttonText}
     </PrismicNextLink> 
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,8 +1,21 @@
+"use client"; // client component is used in this file to run on client side
+
+
+
+
 import { asText, Content } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+
 import { Bounded } from "@/app/components/Bounded";
 import Button from "@/app/components/Button";
+import { TextSplitter } from "@/app/components/TextSplitter";
+
+
+gsap.registerPlugin(useGSAP);
 /**
  * Props for `Hero`.
  */
@@ -12,16 +25,37 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+
+  useGSAP(()=> {
+    const introTl = gsap.timeline();
+
+    introTl
+      .set(".hero", {opacity: 1})
+      .from(".hero-header-word", {
+      duration:0.5,
+      scale:3,
+      opacity:0,
+      ease: "power4.in",
+      delay:0.3,
+    });
+
+  });
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="hero opacity-0"
     >
     <div className="grid">
       <div className="grid h-screen place-items-center">
         <div className="grid auto-rows-min place-items-center text-center">
           <h1 className="hero-header text-7xl font-black uppercase leading-[.8] text-orange-500 md:text[9rem] lg:text-[13rem]">  
-            {asText(slice.primary.heading)} {/*Function returns string instead of h1 within itself*/}
+            <TextSplitter 
+              text={asText(slice.primary.heading)} 
+              wordDisplayStyle="block" //block stacks text
+              className="hero-header-word"/>
+              {/*Function returns string instead of h1 within itself*/}
           </h1>
           <div className="hero subheading mt-12 text-5xl font-semibold text-sky-950 lg:text-6xl">
             <PrismicRichText field={slice.primary.subheading} />
@@ -29,14 +63,25 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           <div className="hero body text-2xl font-normal text-sky-950">
             <PrismicRichText field={slice.primary.body} />
           </div>
-          <Button buttonLink={slice.primary.button_link} buttonText={slice.primary.button_text} className="hero-button mt-12" />
+          <Button 
+            buttonLink={slice.primary.button_link} 
+            buttonText={slice.primary.button_text} 
+            className="hero-button mt-12" />
         </div>
       </div>
   
       <div className="text-side relative z-[80] grid h-screen items-center gap-4 md:grid-cols=2">
-        <PrismicNextImage field={slice.primary.cans_image} />
-        <PrismicRichText field={slice.primary.second_heading} />
-        <PrismicRichText field={slice.primary.second_body} />
+        <PrismicNextImage 
+          className="w-full md:hidden"
+          field={slice.primary.cans_image}/>
+        <div>
+        <h2 className="text-side-heading text-balance text-6xl font-black uppercase text-sky-950 lg:text-8xl">
+          <TextSplitter text={asText(slice.primary.second_heading)} />
+        </h2>
+        <div className="text-side-body mt-4 max-w-xl text-balance text-xl font-normal text-sky-950">
+            <PrismicRichText field={slice.primary.second_body} />
+          </div>
+        </div>  
       </div>
     </div>
     </Bounded>
