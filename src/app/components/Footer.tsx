e469356ac18646dd0b2b65380193cf24b4a395b7
LoopFruits <Kensworldc@gmail.com>
2025-01-03 12:56:09 -0500

    removed use client in footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,21 +1,19 @@
-"use client";
-import React from 'react'
-import { FizzLogo } from './FizzLogo';
-import CircleText from './CircleText';
+import React from "react";
 
-type Props = {}
+import CircleText from "./CircleText";
+import { FizzLogo } from "./FizzLogo";
+
+type Props = {};
 
 export default function Footer({}: Props) {
   return (
     <footer className="bg-[#FEE832] text-[#FE6334]">
-
-        <div className="realtive mx-auto flext w-full max-w-4xl justify-center px-4 py-10">
-            <FizzLogo/>
-            <div className="absolute right-24 top-0 size-28 origin-center -translate-y-14 md:size-48 md:-translate-y-28">
-            {/* circle text */}
-                <CircleText/>
-            </div>
+      <div className="relative mx-auto flex w-full max-w-4xl justify-center px-4 py-10">
+        <FizzLogo />
+        <div className="absolute right-24 top-0 size-28 origin-center -translate-y-14 md:size-48 md:-translate-y-28">
+          <CircleText />
         </div>
+      </div>
     </footer>
-  )
+  );
 }
